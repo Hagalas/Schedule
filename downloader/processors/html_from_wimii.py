6e67247f77a570ea2a0e8a3bd6f051613311beb9
Oskar Marszałek <oskarm91@o2.pl>
2014-07-13 20:15:57 +0200

    processors

---
+++ downloader/processors/html_from_wimii.py
@@ -0,0 +1,10 @@
+
+class HtmlFromWimiiProcessor(object):
+    """
+
+    """
+    def process(self, file_obj):
+        print file_obj.file.read()
+        print "html_from_wimii_processor - test proces ok %s" % file_obj
+
+
